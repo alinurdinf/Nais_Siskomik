67c403fd9d88158834630036849101e7c3a8fd9d
caca-af <caca.amatulfirdausya@gmail.com>
2023-01-04 17:40:07 +0700

    update fraghome, materi, absen (view)

--- app/src/main/java/com/example/railway/rerofit/ApiEndpoint.java
+++ app/src/main/java/com/example/railway/rerofit/ApiEndpoint.java
@@ -8,7 +8,6 @@ import com.example.railway.NilaiModel;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
